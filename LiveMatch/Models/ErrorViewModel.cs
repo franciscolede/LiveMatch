76fce057d6df49f3101b87d612d3144b4aa43bf8
franciscolede <franciscolede01@gmail.com>
2023-11-10 14:27:33 -0300

    Primer commit con los paquetes

---
+++ LiveMatch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LiveMatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
