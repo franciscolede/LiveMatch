b777781ed4372bf3e96496f203639e8dfd93decb
franciscolede <franciscolede01@gmail.com>
2023-11-11 15:51:20 -0300

    Agregar evento primera version

--- LiveMatch/Controllers/HomeController.cs
+++ LiveMatch/Controllers/HomeController.cs
@@ -22,9 +22,105 @@ namespace LiveMatch.Controllers
             return View(await _contexto.Eventos.ToListAsync());
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Evento evento)
+        {
+            if(ModelState.IsValid)
+            {
+                _contexto.Eventos.Add(evento);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit (int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = _contexto.Eventos.Find(id);
+            if(evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Update(evento);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details (int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = _contexto.Eventos.Find(id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = _contexto.Eventos.Find(id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteEvento(int? id)
+        {
+            var evento = await _contexto.Eventos.FindAsync(id);
+            if (evento == null)
+            {
+                return View();
+            }
+
+            _contexto.Eventos.Remove(evento);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
